46876e2a153187b130fcf50c2161f9c7d318eb7a
KashmalaKamal4 <kashmalakamal4@gmail.com>
2024-10-18 08:19:18 -0700

    Add files via upload

---
+++ brief.js
@@ -0,0 +1,34 @@
+ar time = new Date().getHours();
+console.log(time)
+if (time < 12){
+  console.log('Good morning')
+}
+else if (time < 16){
+  console.log('Good afternoon')
+}
+else if (time < 22){
+  console.log('Good evening')
+}
+else{
+  console.log('Good night')
+}
+var i = 1;
+while(i <= 10){
+  console.log(i);
+  i = i + 2;
+}
+var i = 1;
+while(i <= 10){
+  console.log("*");
+  i = i + 2;
+}
+
+var a=5;
+var b=6;
+var c=10;
+var d=null;
+var e;
+console.log(a==b);
+console.log(b!=c);
+console.log(d);
+console.log(e);
